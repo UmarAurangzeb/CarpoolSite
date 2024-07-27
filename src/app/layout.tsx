ea0c27497a443a24ea0246b015fe60f6fac3ef4c
umar aurangzeb <umar12598@gmail.com>
2024-07-28 03:08:00 +0500

    added analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,8 +16,24 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="bg-slate-950">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <>
+      <Head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-NS1QVR2CXT"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-NS1QVR2CXT');
+          `,
+          }}
+        />
+      </Head>
+      <html lang="en" className="bg-slate-950">
+        <body className={inter.className}>{children}</body>
+      </html>
+    </>
   );
 }
