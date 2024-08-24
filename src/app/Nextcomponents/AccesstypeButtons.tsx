af711cef4fd4f78a8c10fc0b986b89c2c3ec185c
umar aurangzeb <umar12598@gmail.com>
2024-08-25 01:00:13 +0500

    some more ui fixess

--- src/app/Nextcomponents/AccesstypeButtons.tsx
+++ src/app/Nextcomponents/AccesstypeButtons.tsx
@@ -19,13 +19,13 @@ export default function AccesstypeButtons() {
     return (
         <div className=' flex flex-row mt-6  mr-2 md:mr-4 h-10'>
             <button
-                className={`text-xs md:text-base max-w-fit w-20 md:max-w-80 md:w-24 mr-2 transition ease-in-out py-0 duration-800 ${activeButton === 'Daily' || activeButton === null ? 'border-2' : 'border-0'}`}
+                className={`text-xs md:text-base w-16 md:max-w-80 md:w-24 mr-2 transition ease-in-out py-0 duration-800 ${activeButton === 'Daily' || activeButton === null ? 'border-2' : 'border-0'}`}
                 onClick={() => handleClick('Daily')}
             >
                 Daily
             </button>
             <button
-                className={`text-xs md:text-base max-w-fit md:w-24 transition ease-in-out duration-800  ${activeButton === 'Semester' ? 'border-2' : 'border-0'}`}
+                className={`text-xs md:text-base w-20 md:w-24 transition ease-in-out duration-800  ${activeButton === 'Semester' ? 'border-2' : 'border-0'}`}
                 onClick={() => handleClick('Semester')}
             >
                 Semester
